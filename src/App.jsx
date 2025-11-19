0536c30e91911abf32630f3ef52495ea37b67060
abhishekverma22 <vermaabhishekabhi22@gmail.com>
2025-11-19 15:08:15 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useMemo } from "react";
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
-import About from "./components/About";
-import Project from "./components/Project";
-import Contact from "./components/Contact";
+import Navbar from "./components/NavBar.jsx";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Project from "./components/Project.jsx";
+import Contact from "./components/Contact.jsx";
 
 const App = () => {
   // Generate bubbles ONCE
